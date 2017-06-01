ee98ca15a4691449944e6ccdf1f6087a087d81ab
vanita22 <vanitayrafita2004@gmail.com>
2017-05-31 21:25:01 -0300

    subiendo archivos con modal en java script que no me resultaron

--- js/app.js
+++ js/app.js
@@ -2,4 +2,28 @@
  * Archivo principal de funcionalidad de JS
  */
 
- 1
+
+(function main(){
+	var boxes = Array.from(document.getElementsByClassName("box-services"));
+	var modal = document.getElementById("box-services-modal");
+	var bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("hide");
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
